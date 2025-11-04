b682b1c68dd9ebcd38c617dfbe253f988a3edfde
Ziling2023 <ziling2023@gmail.com>
2025-11-04 10:30:31 +0000

    main

--- scripts/submit_main.sh
+++ scripts/submit_main.sh
@@ -25,36 +25,34 @@ LOG_PATH="gs://$BUCKET2/logs/main_${MODEL}.log"
 TRAINSET_PATH="gs://$BUCKET2/train_withds"
 TESTSET_PATH="gs://$BUCKET2/test_withds"
 
-NUM_WORKERS=2
-MASTER_DISK=100
-WORKER_DISK=100
+# NUM_WORKERS=2
+# MASTER_DISK=100
+# WORKER_DISK=100
 
-# ---------------------------
-# 创建 Dataproc 集群
-# ---------------------------
-gcloud dataproc clusters create $CLUSTER_NAME \
-  --region=$REGION \
-  --num-workers=$NUM_WORKERS \
-  --worker-machine-type=n2-standard-4 \
-  --master-machine-type=n2-standard-4 \
-  --master-boot-disk-size=$MASTER_DISK \
-  --worker-boot-disk-size=$WORKER_DISK \
-  --image-version="2.2-debian12" \
-  --optional-components=JUPYTER \
-  --enable-component-gateway
+# gcloud dataproc clusters create $CLUSTER_NAME \
+#   --region=$REGION \
+#   --num-workers=$NUM_WORKERS \
+#   --worker-machine-type=n2-standard-4 \
+#   --master-machine-type=n2-standard-4 \
+#   --master-boot-disk-size=$MASTER_DISK \
+#   --worker-boot-disk-size=$WORKER_DISK \
+#   --image-version="2.2-debian12" \
+#   --optional-components=JUPYTER \
+#   --enable-component-gateway
 
-# ---------------------------
-# 提交 PySpark 任务
-# ---------------------------
-gcloud dataproc jobs submit pyspark "$SCRIPT_PATH" \
+zip -r src.zip ./src
+
+gcloud dataproc jobs submit pyspark \
+    test/main.py \
   --cluster=$CLUSTER_NAME \
   --region=$REGION \
-  --py-files="src/" \
+  --py-files=src.zip \
   --jars="gs://hadoop-lib/gcs/gcs-connector-latest-hadoop3.jar" \
   -- \
   --model $MODEL \
   --train-path $TRAINSET_PATH \
   --test-path $TESTSET_PATH \
+  --sample-fraction=0.001 \
   --num-folds 4 \
   > >(tee /tmp/job_${MODEL}.log) 2>&1
 
